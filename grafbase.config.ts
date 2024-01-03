3a59866987d7c9ae9cbda2e99e851e08c7ece9bf
Javad <Javad.esmatib@Gmail.com>
2024-01-03 10:46:54 +0100

    grafbase config

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,4 +1,4 @@
-import { graph, config} from '@grafbase/sdk'
+/* import { graph, config} from '@grafbase/sdk'
 
 // Welcome to Grafbase!
 //
@@ -40,3 +40,48 @@ export default config({
   //   ]
   // }
 })
+ */
+
+import { graph, config, auth } from '@grafbase/sdk';
+import { Graph } from '@grafbase/sdk/dist/src/grafbase-schema';
+const g = graph.Standalone()
+// @ts-ignore
+const User = Graph.model('User', {
+  name: g.string().length({ min: 2, max: 100 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(),
+  projects: g.relation(() => Project).list().optional(),
+}).auth((rules: { public: () => { (): any; new(): any; read: { (): void; new(): any; }; }; }) => {
+  rules.public().read()
+})
+
+// @ts-ignore
+const Project = Graph.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+}).auth((rules: { public: () => { (): any; new(): any; read: { (): void; new(): any; }; }; private: () => { (): any; new(): any; create: { (): { (): any; new(): any; delete: { (): { (): any; new(): any; update: { (): void; new(): any; }; }; new(): any; }; }; new(): any; }; }; }) => {
+  rules.public().read()
+  rules.private().create().delete().update()
+})
+
+const jwt = auth.JWT({
+  issuer: 'grafbase',
+  secret: g.env('NEXTAUTH_SECRET')
+})
+
+export default config({
+  schema: g,
+  auth: {
+    providers: [jwt],
+    rules: (rules) => rules.private()
+  },
+})
+
